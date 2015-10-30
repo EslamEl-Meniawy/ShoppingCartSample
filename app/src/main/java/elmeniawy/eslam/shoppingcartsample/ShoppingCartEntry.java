92fe16263340d7f420251946e319c94c6c92d1d2
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2015-10-30 13:53:31 +0200

    Add ShoppingCartEntry.java

---
+++ app/src/main/java/elmeniawy/eslam/shoppingcartsample/ShoppingCartEntry.java
@@ -0,0 +1,26 @@
+package elmeniawy.eslam.shoppingcartsample;
+
+/**
+ * Created by Eslam El-Meniawy on 30-Oct-15.
+ */
+public class ShoppingCartEntry {
+    private Product mProduct;
+    private int mQuantity;
+
+    public ShoppingCartEntry(Product product, int quantity) {
+        mProduct = product;
+        mQuantity = quantity;
+    }
+
+    public Product getProduct() {
+        return mProduct;
+    }
+
+    public int getQuantity() {
+        return mQuantity;
+    }
+
+    public void setQuantity(int quantity) {
+        mQuantity = quantity;
+    }
+}
